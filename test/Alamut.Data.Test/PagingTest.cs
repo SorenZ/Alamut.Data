62c5f4ab07797943cb00d71e90831349b2d1be53
Hamed ZVand <sunrise.freedom@gmail.com>
2019-11-04 10:07:16 +0330

    Alamut.Data.Test project added

---
+++ test/Alamut.Data.Test/PagingTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Alamut.Data.Test
+{
+    public class PagingTest
+    {
+        [Fact]
+        public void PaginatedExtensions_GetPaginated_ReturnPaginatedResult()
+        {
+            
+        }
+    }
+}
