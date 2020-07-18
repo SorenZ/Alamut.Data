72d3ec2283919768893f5296212c6215366af1cc
Hamed ZVand <sunrise.freedom@gmail.com>
2020-07-18 12:20:42 +0430

    change ToPaginatedAsync from parallel to async

--- src/Alamut.Data/Paging/QueryablePaginatedExtensions.cs
+++ src/Alamut.Data/Paging/QueryablePaginatedExtensions.cs
@@ -57,16 +57,17 @@ namespace Alamut.Data.Paging
         public static async Task<IPaginated<T>> ToPaginatedAsync<T>(this IQueryable<T> query, 
             IPaginatedCriteria paginatedCriteria, CancellationToken cancellationToken = default)
         {
-            var dataTask = query.ToPage(paginatedCriteria)
+            var data = await query.ToPage(paginatedCriteria)
                 .ToListAsync(cancellationToken);
 
-            var countTask = query.CountAsync(cancellationToken);
+            var count = await query.CountAsync(cancellationToken);
 
-            await Task.WhenAll(dataTask, countTask);
+            // await Task.WhenAll(dataTask, countTask);
+            // "A second operation started on this context before a previous operation completed. This is usually caused by different threads using the same instance of DbContext. For more information on how to avoid threading issues with DbContext, see https://go.microsoft.com/fwlink/?linkid=2097913.
 
             return new Paginated<T>(
-                dataTask.Result,
-                countTask.Result,
+                data,
+                count,
                 paginatedCriteria.CurrentPage,
                 paginatedCriteria.PageSize);
         }
